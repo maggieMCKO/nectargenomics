6797b9d5bf87ad5a52693810602b005d4ef1eadb
maggieMCKO <mengchingko@gmail.com>
2020-05-02 14:06:52 +0200

    wget assembly_report.txt

--- CNEEanalysis/01_phylofit/fix_galgalanno.sh
+++ CNEEanalysis/01_phylofit/fix_galgalanno.sh
@@ -8,7 +8,7 @@ fixed=${input_ann%%.gff}_chrtabFixed.gff
 
 # get acc
 wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/002/315/GCF_000002315.6_GRCg6a/GCF_000002315.6_GRCg6a_assembly_report.txt
-sed 's/\r$//g' GCF_000002315.6_GRCg6a_assembly_report.txt | grep -v "^#" | cut -f7,10 | awk '{gsub(/chr/, "galGal6.chr") ; print}' > ${trans_matrix}
+sed 's/\r$//g' GCF_000002315.6_GRCg6a_assembly_report.txt | grep -v "^#" | cut -f7,10 | awk '{gsub(/chr/, "galGal6.chr") ; print}' | awk '{gsub(/\tna/, "\tgalGal6.MT") ; print}' > ${trans_matrix}
 
 # fix
 perl replace_chrs.pl ${trans_matrix} ${input_ann} > ${fixChr}
