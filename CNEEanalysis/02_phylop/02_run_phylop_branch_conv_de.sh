0d1d497390a716bbd259a2e2c4bc25d7917bf24f
maggieMCKO <mengchingko@gmail.com>
2020-05-19 18:09:50 +0200

    update phyloP scripts

---
+++ CNEEanalysis/02_phylop/02_run_phylop_branch_conv_de.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#SBATCH --time=0-06:00:00
+#SBATCH -n 1 # or --ntasks=1
+#SBATCH --mem=20g
+#SBATCH --partition=medium
+#SBATCH -o phylopBC_%A.out
+#SBATCH -e phylopBC_%A.err
+#SBATCH -J phylopBC
+#SBATCH --mail-type=ALL        # BEGIN,END,FAIL,ALL
+#SBATCH --mail-user=mcko@orn.mpg.de
+
+##### SETUP INPUT and OUTPUT
+cneeAna_DIR="/home/mpg08/mko/Nectar/analysis/CNEEanalysis/"
+nput_tree="${cneeAna_DIR}NectarTree01.nw"
+chicken_anno="${cneeAna_DIR}GCF_000002315.6_GRCg6a_genomic_chrtabFixed2.gff" # chr need to match maf's chr with species (galgal.chrx)
+
+maf_DIR="${cneeAna_DIR}MAF/"
+input_maf="${maf_DIR}multi.anno.maf"
+maf_processed_DIR="${maf_DIR}Processed"
+
+phylofit_DIR="${cneeAna_DIR}01_phylofit/01_msa3/"
+neutral_model="${phylofit_DIR}nonconserved_4d.mod"
+
+phylop_DIR="${cneeAna_DIR}02_phylop/"
+features_bed="${phylop_DIR}galGal6_final_merged_CNEEs_named_fixchr2.bed"
+features_out="${phylop_DIR}features.out"
+
+WD="${cneeAna_DIR}02_phylop/subtree2/"
+neutral_model_named="${WD}nonconserved_4d_named.mod"
+
+tmp_branch="HLlicCas1-HLacaPus1,HLtriMol2,HLcalAnn5-HLfloFus1"
+echo ${tmp_branch}
+
+##### SETUP programs
+export phastPATH="/home/mpg08/mko/Tools/phast/bin/"
+export phyloFit_PATH="${phastPATH}phyloFit"
+export msa_view_PATH="${phastPATH}msa_view"
+export phyloP_PATH="${phastPATH}phyloP"
+export tree_doctor_PATH="${phastPATH}tree_doctor"
+
+
+##### RUN
+### Run phyloP with the branch options.
+${phyloP_PATH} --method LRT --branch ${tmp_branch} --mode CONACC --msa-format MAF --features ${features_bed} ${neutral_model_named} ${input_maf} > "branch_${tmp_branch}_cnee.wig"
