05cf529aba01136d1cefec59ef265516a80e1fa1
Maggie Ko <mengchingko@gmail.com>
2022-01-20 21:05:06 +0800

    update scripts 1 & 3

--- CNEEanalysis_2021/01b_cnee_prep/1_getcnees.sh
+++ CNEEanalysis_2021/01b_cnee_prep/1_getcnees.sh
@@ -4,7 +4,8 @@
 # based on: https://github.com/sjswuitchik/duck_comp_gen/blob/master/03a_cnee_analysis/01_assemble_cnees.sh
 
 ### SETUP
-export cneeAna_DIR="$HOME/Nectar/analysis/CNEEanalysis_2021/"
+# export cneeAna_DIR="$HOME/Nectar/analysis/CNEEanalysis_2021/"
+export cneeAna_DIR="../"
 
 module purge
 module load bedtools/2.29.1
@@ -13,31 +14,22 @@ module load bedtools/2.29.1
 mkdir -p cnee_len_ana
 mkdir -p bed_outputs
 
-get cnee_len_ana/ce.lengths
 awk '{print $3-$2, "\tLowe"}' "${cneeAna_DIR}00_inputs/cnee_bedfiles/galGal6_Lowe_merged.bed" >> cnee_len_ana/ce.lengths
 awk '{print $3-$2, "\tSackton"}' "${cneeAna_DIR}00_inputs/cnee_bedfiles/galGal6_Sackton_merged.bed"  >> cnee_len_ana/ce.lengths
 awk '{print $3-$2, "\tUCSC"}' "${cneeAna_DIR}00_inputs/cnee_bedfiles/galGal6_UCSC_merged.bed" >> cnee_len_ana/ce.lengths
 awk '{print $3-$2, "\tCraig"}' "${cneeAna_DIR}00_inputs/cnee_bedfiles/galGal6_Craig_merged.bed" >> cnee_len_ana/ce.lengths
 
+# remove cnees larger than 5000 bp, then merge cnee sets
+awk '{if ($3-$2 <= 5000) print $0}' "${cneeAna_DIR}00_inputs/cnee_bedfiles/galGal6_Craig_merged.bed" > bed_outputs/galGal6_Craig_filtered.bed
+awk '{if ($3-$2 <= 5000) print $0}' "${cneeAna_DIR}00_inputs/cnee_bedfiles/galGal6_Lowe_merged.bed" > bed_outputs/galGal6_Lowe_filtered.bed
+awk '{if ($3-$2 <= 5000) print $0}' "${cneeAna_DIR}00_inputs/cnee_bedfiles/galGal6_Sackton_merged.bed"  > bed_outputs/galGal6_Sackton_filtered.bed
+awk '{if ($3-$2 <= 5000) print $0}' "${cneeAna_DIR}00_inputs/cnee_bedfiles/galGal6_UCSC_merged.bed" > bed_outputs/galGal6_UCSC_filtered.bed
+
 # merge cnee sets
 for d in 0 1 2 3 4 5 6 7 8 9 10
 do
-cat ${cneeAna_DIR}00_inputs/cnee_bedfiles/galGal6_*_merged.bed | bedtools sort -i - | bedtools merge -i - -d $d > bed_outputs/galGal6_all_merged_${d}bp.bed
-
-awk '{print $3-$2}' bed_outputs/galGal6_all_merged_${d}bp.bed > cnee_len_ana/galGal6_all_merged_${d}bp.lengths
+  out_bed="bed_outputs/galGal6_all_filtered_merged_${d}bp.bed"
+  out_len="cnee_len_ana/galGal6_all_filtered_merged_${d}bp.lengths"
+  cat bed_outputs/galGal6_*_filtered.bed | bedtools sort -i - | bedtools merge -i - -d $d > ${out_bed}
+  awk '{print $3-$2}' ${out_bed} > ${out_len}
 done
-
-# remove cnees larger than 1000 then merge cnee sets
-awk '{if ($3-$2 <= 1000) print $0}' "${cneeAna_DIR}00_inputs/cnee_bedfiles/galGal6_Lowe_merged.bed" > bed_outputs/galGal6_Lowe_merged_eqless1000.bed
-awk '{if ($3-$2 <= 1000) print $0}' "${cneeAna_DIR}00_inputs/cnee_bedfiles/galGal6_Sackton_merged.bed"  > bed_outputs/galGal6_Sackton_merged_eqless1000.bed
-awk '{if ($3-$2 <= 1000) print $0}' "${cneeAna_DIR}00_inputs/cnee_bedfiles/galGal6_UCSC_merged.bed" > bed_outputs/galGal6_UCSC_merged_eqless1000.bed
-awk '{if ($3-$2 <= 1000) print $0}' "${cneeAna_DIR}00_inputs/cnee_bedfiles/galGal6_Craig_merged.bed" > bed_outputs/galGal6_Craig_merged_eqless1000.bed
-
-for d in 0 1 2 3 4 5 6 7 8 9 10
-do
-cat bed_outputs/galGal6_*_merged_eqless1000.bed | bedtools sort -i - | bedtools merge -i - -d $d > bed_outputs/galGal6_all_merged_eqless1000_${d}bp.bed
-
-awk '{print $3-$2}' bed_outputs/galGal6_all_merged_eqless1000_${d}bp.bed > cnee_len_ana/galGal6_all_merged_eqless1000_${d}bp.lengths
-done
-
-# | awk '{if ($3-$2 >= 50) print $0}'
--- CNEEanalysis_2021/01b_cnee_prep/3_annocnees.sh
+++ CNEEanalysis_2021/01b_cnee_prep/3_annocnees.sh
@@ -4,11 +4,15 @@
 # based on: https://github.com/sjswuitchik/duck_comp_gen/blob/master/03a_cnee_analysis/01_assemble_cnees.sh
 
 ### SETUP
-export cneeAna_DIR="$HOME/Nectar/analysis/CNEEanalysis_2021/"
+export cneeAna_DIR="../"
 export chicken_anno="${cneeAna_DIR}00_inputs/GCF_000002315.6_GRCg6a_genomic.gff" # ori
 
 export replace_chrs_perl="${cneeAna_DIR}01a_phylofit/01_gff_prep/replace_chrs.pl"
 export trans_matrix_for_maf="${cneeAna_DIR}01a_phylofit/01_gff_prep/galgal6a_2315.6_acc_chr_matrix.tsv" # (chr)
+
+export merged_bed="bed_outputs/galGal6_all_filtered_merged_5bp.bed" # input
+export merged_bed_filtered="bed_outputs/galGal6_all_filtered_merged_5bp_filtered.bed" # output
+export merged_bed_filtered_len="cnee_len_ana/galGal6_all_filtered_merged_5bp_filtered.lengths" # output
 
 module purge
 module load bedtools/2.29.1
@@ -19,8 +23,12 @@ module load bedtools/2.29.1
 # gunzip GCF_000002315.6_GRCg6a_genomic.gff.gz
 awk 'BEGIN{OFS="\t";} {if ($3 ~ /exon/) print $1, $4-1, $5}' ${chicken_anno} | bedtools sort -i - | bedtools merge -i - > bed_outputs/galGal6.exon.bed
 
+# filte small (<50bp) CNEEs
+awk '{if ($3-$2 >= 50) print $0}' ${merged_bed} > ${merged_bed_filtered}
+awk '{print $3-$2}' ${merged_bed_filtered} > ${merged_bed_filtered_len}
+
 # get CNEEs
-bedtools intersect -v -a bed_outputs/galGal6_all_merged_eqless1000_5bp.bed -b bed_outputs/galGal6.exon.bed > bed_outputs/galGal6_final_merged_CNEEs.bed
+bedtools intersect -v -a ${merged_bed_filtered} -b bed_outputs/galGal6.exon.bed > bed_outputs/galGal6_final_merged_CNEEs.bed
 
 # add names
 awk 'BEGIN {FS="\t"; OFS="\t"} {$4="CNEE"NR; print}' bed_outputs/galGal6_final_merged_CNEEs.bed > bed_outputs/galGal6_final_merged_CNEEs_named.bed
